44923593aa85a616ab707013bbb6216790c16923
Sensus Media <36053809+sensusmedia@users.noreply.github.com>
2018-12-13 12:36:35 -0700

    Add files via upload

---
+++ vivus-custom.js
@@ -0,0 +1,135 @@
+(function() {
+    var logo = new Vivus('layer4', {
+        type: 'delayed',
+        forceRender: false,
+        duration: 150
+    }, fill);
+    var lightgrey = document.getElementById('lightgrey');
+    var lightgrey2 = document.getElementById('lightgrey2');
+    var lightgrey3 = document.getElementById('lightgrey3');
+    var lightgrey4 = document.getElementById('lightgrey4');
+    var orange = document.getElementById('orange');
+    var orange2 = document.getElementById('orange2');
+    var orange3 = document.getElementById('orange3');
+    var white = document.getElementById('white');
+    var white2 = document.getElementById('white2');
+    var red = document.getElementById('red');
+    var red2 = document.getElementById('red2');
+    var red3 = document.getElementById('red3');
+    var blue = document.getElementById('blue');
+
+    function fill() {
+        lightgrey.style.fill = '#e6eaea';
+        lightgrey.style.stroke = 'none';
+        lightgrey2.style.fill = '#9baaab';
+        lightgrey2.style.stroke = 'none';
+        lightgrey3.style.fill = '#cdd4d5';
+        lightgrey3.style.stroke = 'none';
+        lightgrey4.style.fill = '#687f82';
+        lightgrey4.style.stroke = 'none';
+        orange.style.fill = '#000080';
+        orange.style.stroke = 'none';
+        orange2.style.fill = '#000080';
+        orange2.style.stroke = 'none';
+        orange3.style.fill = '#000080';
+        orange3.style.stroke = 'none';
+        white.style.fill = '#ffffff';
+        white.style.stroke = 'none';
+        white2.style.fill = '#ffffff';
+        white2.style.stroke = 'none';
+        red.style.fill = '#00f';
+        red.style.stroke = 'none';
+        red2.style.fill = '#00f';
+        red2.style.stroke = 'none';
+        red3.style.fill = '#00f';
+        red3.style.stroke = 'none';
+        blue.style.fill = '#4acfd9';
+        blue.style.stroke = 'none';
+    }
+})();
+(function() {
+    var logo = new Vivus('layer5', {
+        type: 'delayed',
+        forceRender: false,
+        duration: 150
+    }, fill);
+    var black = document.getElementById('black');
+    var teal = document.getElementById('teal');
+    var teal2 = document.getElementById('teal2');
+    var lightgrey5 = document.getElementById('lightgrey5');
+    var lightgrey6 = document.getElementById('lightgrey6');
+    var lightgrey7 = document.getElementById('lightgrey7');
+    var lightgrey8 = document.getElementById('lightgrey8');
+    var lightgrey9 = document.getElementById('lightgrey9');
+    var lightgrey10 = document.getElementById('lightgrey10');
+    var white3 = document.getElementById('white3');
+    var white4 = document.getElementById('white4');
+    var red4 = document.getElementById('red4');
+    var orange4 = document.getElementById('orange4');
+
+    function fill() {
+        black.style.fill = '#365558';
+        black.style.stroke = 'none';
+        teal.style.fill = '#4ACFD9';
+        teal.style.stroke = 'none';
+        teal2.style.fill = '#0295AA';
+        teal2.style.stroke = 'none';
+        lightgrey5.style.fill = '#9BAAAB';
+        lightgrey5.style.stroke = 'none';
+        lightgrey6.style.fill = '#687F82';
+        lightgrey6.style.stroke = 'none';
+        lightgrey7.style.fill = '#E6EAEA';
+        lightgrey7.style.stroke = 'none';
+        lightgrey8.style.fill = '#CDD4D5';
+        lightgrey8.style.stroke = 'none';
+        lightgrey9.style.fill = '#9BAAAB';
+        lightgrey9.style.stroke = 'none';
+        lightgrey10.style.fill = '#365558';
+        lightgrey10.style.stroke = 'none';
+        white3.style.fill = '#ffffff';
+        white3.style.stroke = 'none';
+        white4.style.fill = '#ffffff';
+        white4.style.stroke = 'none';
+        red4.style.fill = '#00f';
+        red4.style.stroke = 'none';
+        orange4.style.fill = '#000080';
+        orange4.style.stroke = 'none';
+    }
+})();
+(function() {
+    var logo = new Vivus('layer6', {
+        type: 'delayed',
+        forceRender: false,
+        duration: 150
+    }, fill);
+    var orange5 = document.getElementById('orange5');
+    var teal3 = document.getElementById('teal3');
+    var lightgrey11 = document.getElementById('lightgrey11');
+    var lightgrey12 = document.getElementById('lightgrey12');
+    var lightgrey13 = document.getElementById('lightgrey13');
+    var lightgrey14 = document.getElementById('lightgrey14');
+    var white5 = document.getElementById('white5');
+    var white6 = document.getElementById('white6');
+    var red5 = document.getElementById('red5');
+
+    function fill() {
+        orange5.style.fill = '#00f';
+        orange5.style.stroke = 'none';
+        teal3.style.fill = '#4ACFD9';
+        teal3.style.stroke = 'none';
+        lightgrey11.style.fill = '#365558';
+        lightgrey11.style.stroke = 'none';
+        lightgrey12.style.fill = '#365558';
+        lightgrey12.style.stroke = 'none';
+        lightgrey13.style.fill = '#365558';
+        lightgrey13.style.stroke = 'none';
+        lightgrey14.style.fill = '#365558';
+        lightgrey14.style.stroke = 'none';
+        white5.style.fill = '#ffffff';
+        white5.style.stroke = 'none';
+        white6.style.fill = '#ffffff';
+        white6.style.stroke = 'none';
+        red5.style.fill = '#000080';
+        red5.style.stroke = 'none';
+    }
+})();
